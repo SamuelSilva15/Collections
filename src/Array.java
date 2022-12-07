50bd378ab782adb2f281d6e96f44a443b99cfb58
Samuel Araújo da Silva <silvamsamuel15@gmail.com>
2022-12-06 19:48:48 -0600

    ArrayList

--- src/Array.java
+++ src/Array.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 
 public class Array {
     public static void main(String[] args) {
+        //aprendendo arraylist
        String aula1 = "Conhecendo mais de listas";
        String aula2 = "Modelando a classe aula";
        String aula3 = "Trabalhando com Cursos e Sets";
